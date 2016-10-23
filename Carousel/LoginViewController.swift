7227d357c365349a586f3aafc1f871a21f9d5a44
sophiakc <sophiakc@gmail.com>
2016-10-23 16:54:47 -0700

    milestone #3: navbar ok

---
+++ Carousel/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  Carousel
+//
+//  Created by Sophia KC on 23/10/2016.
+//  Copyright © 2016 Sophia KC. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+}
