572663c80df4c31817c5e192a23204920d360c5f
sophiakc <sophiakc@gmail.com>
2016-10-23 18:35:20 -0700

    milestone #3: still error message pending with loginScrollView.delegate = self

--- Carousel/LoginViewController.swift
+++ Carousel/LoginViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    
+    
+    @IBOutlet weak var loginScrollView: UIScrollView!
+    
+    @IBAction func didPressBack(_ sender: AnyObject) {
+        navigationController!.popViewController(animated: true)
+    }
+    
+    
+    class LoginViewController: UIViewController, UIScrollViewDelegate {
+        
+        func scrollViewDidScroll(_ loginScrollView: UIScrollView) {
+            // This method is called as the user scrolls
+        }
         
     }
 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+        // tell the scroll view to call the view controller with the scroll view event methods
+//        loginScrollView.delegate = self
+        
+        // Set the scroll view content size
+        loginScrollView.contentSize = loginScrollView.frame.size
+        
+        // Set the content insets
+        loginScrollView.contentInset.bottom = 100
+
+        
+    }
+    
+    
     
 }
